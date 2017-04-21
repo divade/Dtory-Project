bb9ab79056e61c168cae409e2c0ab6e4683342d4
David Krief <d.krief@live.fr>
2017-04-21 14:30:59 +0200

    ajustement des routes

--- projetdtory/src/index.js
+++ projetdtory/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
 import App from './App';
 import mainpage from './Page/mainpage/mainpage';
 import homepage from './Page/HomePage/homepage';
@@ -10,8 +9,8 @@ import './index.css';
 ReactDOM.render(
   <Router>
     <div>
-      <Route exact path='/' component={mainpage} />
-      <Route path='/homepage' component={homepage} />
+      <Route exact path='/' component={homepage} />
+      <Route path='/mainpage' component={mainpage} />
     </div>
   </Router>,
   document.getElementById('root')
